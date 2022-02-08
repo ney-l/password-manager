baf5083d293c4990e0085abc7a45fd5cd1087bef
Ney Lanjewar <neylanjewar@gmail.com>
2022-02-08 18:54:29 +0530

    Filters out "problematic" words

--- password_generator.py
+++ password_generator.py
@@ -9,16 +9,23 @@ class PasswordGenerator:
         self.word_list = []
         self.long_words = []
         self.short_words = []
+        self.problematic_words = []
 
     def get_random_words(self):
         if len(self.word_list) > 0:
             return self.pick_words()
 
-        response = requests.get(URL)
-        self.word_list = response.text.split("\n")
-        self.short_words = [word for word in self.word_list if len(word) <= 6]
-        self.long_words = [word for word in self.word_list if 6 < len(word) < 12]
+        self.fetch_words()
         return self.pick_words()
+
+    def fetch_words(self):
+        with open("problematic_words.txt") as bad_words_file:
+            self.problematic_words = [word.strip("\n") for word in bad_words_file.readlines()]
+        response = requests.get(URL)
+        # filter out "problematic" words
+        self.word_list = [word for word in response.text.split("\n") if word not in self.problematic_words]
+        self.short_words = [word for word in self.word_list if 3 < len(word) <= 6]
+        self.long_words = [word for word in self.word_list if 6 < len(word) < 10]
 
     def pick_words(self):
         random_short_words = choices(self.short_words, k=2)
